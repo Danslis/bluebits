7d99827855c9281dece0c02496533d99b0378ff5
Dart <Danslis@yandex.ru>
2023-02-25 21:03:41 +0300

    доработал приложение

---
+++ apps/ngshop/src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { ProductListComponent } from './pages/product-list/product-list.component';
+import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+//import { RouterModule, Routes } from '@angular/router';
+// import { HeaderComponent } from './shared/header/header.component';
+// import { FooterComponent } from './shared/footer/footer.component';
+// import { UiModule } from '@bluebits/ui';
+
+const routes: Routes = [
+  { path: '', component: HomePageComponent },
+  { path: 'products', component: ProductListComponent },
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomePageComponent,
+    ProductListComponent,
+    HeaderComponent,
+    FooterComponent,
+  ],
+  imports: [BrowserModule, RouterModule.forRoot(routes)],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
