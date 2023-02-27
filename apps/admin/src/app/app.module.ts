c5c86a7539d4a3f2c7ff52bbf745e4b608285160
Dart <Danslis@yandex.ru>
2023-02-27 23:55:38 +0300

    как работает страница категории

--- apps/admin/src/app/app.module.ts
+++ apps/admin/src/app/app.module.ts
@@ -1,27 +1,77 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { RouterModule, Routes } from '@angular/router';
 import { ShellComponent } from './shared/shell/shell.component';
 import { SidebarComponent } from './shared/sidebar/sidebar.component';
 
+import { CardModule } from 'primeng/card';
+import { ToolbarModule } from 'primeng/toolbar';
+import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
+import { CategoriesService } from '@bluebits/products';
+
+import { InputTextModule } from 'primeng/inputtext';
+import { ToastModule } from 'primeng/toast';
+import { ConfirmationService, MessageService } from 'primeng/api';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { ColorPickerModule } from 'primeng/colorpicker';
+import { CategoriesListComponent } from './categories/categories-list/categories-list.component';
+import { CategoriesFormComponent } from './categories/categories-form/categories-form.component';
+
+const UX_MODULE = [
+  CardModule,
+  ToastModule,
+  InputTextModule,
+  TableModule,
+  ToolbarModule,
+  ButtonModule,
+  ConfirmDialogModule,
+  ColorPickerModule
+];
+
 const routes: Routes = [
   {
     path: '',
     component: ShellComponent,
-    pathMatch: 'full',
-  },
+    children: [
+      {
+        path: 'categories',
+        component: CategoriesListComponent
+      },
+      {
+        path: 'categories/form',
+        component: CategoriesFormComponent
+      },
+      {
+        path: 'categories/form/:id',
+        component: CategoriesFormComponent
+      }
+    ]
+  }
 ];
 
 @NgModule({
-  declarations: [AppComponent, SidebarComponent, ShellComponent],
+  declarations: [
+    AppComponent,
+    SidebarComponent,
+    ShellComponent,
+    CategoriesListComponent,
+    CategoriesFormComponent
+  ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(routes, {  initialNavigation: 'enabledBlocking'}),
+    BrowserAnimationsModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(routes, { initialNavigation: 'enabledBlocking' }),
+    ...UX_MODULE
   ],
-  providers: [],
-  bootstrap: [AppComponent],
+  providers: [CategoriesService, MessageService, ConfirmationService],
+  bootstrap: [AppComponent]
 })
-
 export class AppModule {}
